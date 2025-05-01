2c390996070c8baf0f4e4c1a2deef08aa0e10222
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2025-05-01 11:48:22 +0200

    alldiff

--- theories/AllDifferent.v
+++ theories/AllDifferent.v
@@ -80,7 +80,77 @@ Definition alldifferent_decide (constraint : AllDifferentConstraint) (a : Assign
   dec_all_different constraint.(vars_ad) valuation
 .
 
+(* Definition AllDifferent (c : sstr.t) (v : string -> Z) : Prop :=
+  (forall (x y : string), 
+  x <> y -> sstr.In x c -> sstr.In y c -> v x <> v y).
+ *)
 
+Definition FullDomain := sint.t.
+
+(* Note that we materialize each domain because otherwise computing the shared domain is quite expensive. *)
+Definition state := string -> FullDomain.
+
+
+
+(* Record BoundedDomain := {
+  bd_lb : Z;
+  bd_ub : Z;
+  bd_holes : sint.t
+}.
+
+Definition dom_size (dom : BoundedDomain) : Z :=
+  dom.(bd_ub) - dom.(bd_lb) + 1 - (Z.of_nat (sint.cardinal dom.(bd_holes))).
+ *)
+Definition dom_size (dom : FullDomain) : nat := sint.cardinal dom.
+
+Definition vars_dom_union (st : state) (vs : sstr.t) := 
+    sstr.fold 
+    (fun x acc => sint.union (st x) acc)
+    vs 
+    sint.empty.
+
+Definition vars_len (vs : sstr.t) : nat := sstr.cardinal vs.
+
+Open Scope nat_scope.
+
+Definition ex_confl_set (st : state) (vs : sstr.t) := 
+  exists confl_vars, 
+    sstr.Subset confl_vars vs 
+      /\ 
+    dom_size (vars_dom_union st confl_vars) < vars_len confl_vars.
+
+Definition valuation_in_state (st : state) (vs : sstr.t) (v : string -> Z) : Prop :=
+  forall (x : string), sstr.In x vs -> sint.In (v x) (st x).
+
+Definition AllDifferent_sat (st : state) (vs : sstr.t) :=
+  exists v, valuation_in_state st vs v -> AllDifferent vs v.
+
+Lemma conflict_if_ex_confl_set (st : state) (vs : sstr.t) :
+  ex_confl_set st vs -> ~ AllDifferent_sat st vs.
+Proof.
+  intros H.
+  destruct H as [confl_vars [Hsub Hlt]].
+  unfold AllDifferent_sat. intros Halldiff.
+  destruct Halldiff as [v [Hdom Hdiff]].
+  enough (dom_size (vars_union_domain st confl_vars) >= vars_len confl_vars).
+  - apply Nat.lt_nge in Hlt.
+    contradiction.
+  - apply all_different_subset with (vs' := confl_vars) in Hdiff; try assumption.
+    destruct (alldiff_valuation_values_props confl_vars st v Hdiff) as [Hvals Hins].
+    rewrite Hvals.
+    unfold ge.
+    apply sint_prps.subset_cardinal.
+    unfold sint.Subset.
+    intros n Hin.
+    destruct (Hins n Hin)  as [x [Hxin Hvx]].
+    subst n.
+    specialize (vars_union_domain_correct st confl_vars x Hxin) as Hunion.
+    autounfold with short_unfold_db in *.
+    apply Hunion.
+    apply Hdom.
+    apply Hsub.
+    exact Hxin.
+Qed.
 
 (* Definition ex_confl_set (st : state) (vs : vars) := 
 (exists (confl_vars : vars), sstr.Subset confl_vars vs /\ dom_size (vars_union_domain st confl_vars) < vars_len confl_vars). *)
